eef6d9e295df0b1894445abfacb6bf32ef42a425
johnrandallia <eiselem9@gmail.com>
2025-03-17 16:04:27 +0000

    added date, changed webpage title and header

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -140,7 +140,7 @@ export default function AboutPage() {
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
                 <h2 className="text-3xl font-bold tracking-tighter md:text-4xl">
-                  Join Us in Barcelona
+                  Join Us in Tenerife
                 </h2>
                 <p className="max-w-[700px] text-primary-foreground/80 md:text-xl">
                   Be part of this exciting gathering of minds from around the
